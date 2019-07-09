033d088fedc58bff2aa2c74f47d3762712d02b30
ilakeful <s.iskylake.s@gmail.com>
2019-07-10 01:41:33 +0300

    the replacement of an old github repo link

--- src/main/kotlin/io/iskylake/lakebot/Immutable.kt
+++ src/main/kotlin/io/iskylake/lakebot/Immutable.kt
@@ -24,7 +24,7 @@ object Immutable {
     const val DEFAULT_PREFIX = "lb!"
     const val VERSION = "1.0-BETA3"
     const val PERMISSIONS = 2146958591L
-    const val GITHUB_REPOSITORY = "https://github.com/ISkylakeS/LakeBot"
+    const val GITHUB_REPOSITORY = "https://github.com/ilakeful/LakeBot"
     const val SUPPORT_INVITE = "https://discord.gg/QsTevwF"
     const val USER_AGENT = "Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3416.0 Safari/537.36"
     val LOGGER = LoggerFactory.getLogger("LakeBot")!!
